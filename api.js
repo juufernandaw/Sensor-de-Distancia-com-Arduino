a5fb1476068a6f0d973335ab75721b7023304f1b
Julia Fernanda Werlang <104525139+juufernandaw@users.noreply.github.com>
2023-05-08 21:37:02 -0300

    Create api.js

---
+++ api.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const distancias = [];
+const datas = [];
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// GET request to fetch all books
+app.get('/books', (req, res) => {
+  return res.json(books);
+});
+
+// POST request to add a new book
+// db contex mongo vai aqui para salvar o que vem do arduino
+app.post('/distance', (req, res) => {
+  const {distancia} = req.body; //conteudo da requisição
+  //const {data_hora} = current_datetime = datetime.datetime.now();/
+  distancias.push(distancia);
+  //datas.push(data_hora);
+  return res.json(distancias);
+  
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
