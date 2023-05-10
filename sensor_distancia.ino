c9c248da2d618e1c4f060d8f127e621b1aa6aa08
Diego Goiana <diegovsg96@gmail.com>
2023-05-10 00:44:26 -0300

    Update sensor_distancia.ino

--- sensor_distancia.ino
+++ sensor_distancia.ino
@@ -5,10 +5,18 @@
 
 // ############# VARIABLES ############### //
 
-const char* SSID = "SmeTeste"; // rede wifi
-const char* PASSWORD = "npaz5072"; // senha da rede wifi
+const char* SSID = "CASA CINZA"; // rede wifi
+const char* PASSWORD = "cinzacasa"; // senha da rede wifi
 
-String BASE_URL = "http://127.0.0.1:3000/";// como passar o ip localhost o ip ta errado
+String BASE_URL = "http://192.168.100.11:3000/distance";// como passar o ip localhost 
+
+// declara os pinos e as variáveis
+const int trig = 12;  // D6
+const int echo = 13;  // D7
+long duration;
+int distance;
+int LED = 14; // D5
+
 
 // ############# PROTOTYPES ############### //
 
@@ -26,6 +34,11 @@ HTTPClient http;
 void setup() {
   initSerial();
   initWiFi();
+  pinMode(trig, OUTPUT); // Trig = saída
+  pinMode(echo, INPUT); // Echo = entrada
+  Serial.begin(9600); // Inicia a taxa de velocidade
+  pinMode(LED, OUTPUT); // Led = saída
+  Serial.begin(115200);
 }
 
 // ############### LOOP ################# //
@@ -33,7 +46,32 @@ void setup() {
 void loop() {
   Serial.println("[GET] /sensors - sending request...");
   Serial.println("");
+  // Limpa o trig
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
 
+    // Ao iniciar já liga o trig por 10 microsegundos
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+    // Faz a leitura da echo
+  duration = pulseIn(echo, HIGH);
+
+    // Calcula a distancia
+  distance= duration*0.034/2;
+    
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(800);
+
+  if(distance<100) { // Se o valor for menor que 150 o LED é acionado
+    digitalWrite(LED, HIGH); // LED liga
+
+  } else {
+    digitalWrite(LED, LOW); // Led desliga
+  }
+  // httpREQUEST
   httpRequest("distance"); //rota
 
   Serial.println("");
@@ -58,7 +96,7 @@ void httpRequest(String path)
 String makeRequest(String path)
 {
   http.begin(client, BASE_URL);
-  int httpCode = http.POST("asdfghjkl:senha");// como interporlar para mandar em json, nome:senha como mandar um json no post
+  int httpCode = http.POST("distance:"+String(distance));// como interporlar para mandar em json, nome:senha como mandar um json no post
 
   if (httpCode < 0) {
     Serial.println("request error - " + httpCode);
