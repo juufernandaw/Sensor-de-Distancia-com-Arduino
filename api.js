a3ed006f2e77efce74d262298b80d6ece9caca5c
Diego Goiana <diegovsg96@gmail.com>
2023-05-10 00:44:04 -0300

    atualização

--- api.js
+++ api.js
@@ -4,27 +4,22 @@ const port = 3000;
 const distancias = [];
 const datas = [];
 
+
 // Middleware to parse JSON requests
 app.use(express.json());
 
-// GET request to fetch all books
-app.get('/books', (req, res) => {
-  return res.json(books);
-});
-
-// POST request to add a new book
 // db contex mongo vai aqui para salvar o que vem do arduino
 app.post('/distance', (req, res) => {
-  let body = req.body; //conteudo da requisição
-  console.log(body.distancia);
-  console.log(body.nome);
+  let body = req.body.distance; //conteudo da requisição
+  console.log(body);
+  // console.log(body.distancia);
+  // console.log(body.nome);
   //const {data_hora} = current_datetime = datetime.datetime.now();/
-  distancias.push(distancia);
+  distancias.push(body);
   //datas.push(data_hora);
   return res.json(distancias);
   
 });
-
 
 // Start the server
 app.listen(port, () => {
